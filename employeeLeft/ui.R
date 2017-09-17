fa35bd00846bbe32af7f7909352e1ad856b5b056
Ansih Singh Walia <loveeanish@gmail.com>
2017-09-18 00:24:45 +0530

    making the app UI

--- employeeLeft/ui.R
+++ employeeLeft/ui.R
@@ -13,21 +13,33 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Human Resource Analytics"),
+  h4("Web app to predict whether an employee will leave the company or not based on the inputs below."),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
+    
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+       numericInput("satisfaction",
+                   "Enter the satisfaction level-(0 to 1)",value=0.25) ,
+       br(),
+       numericInput("evaluation",
+                    "Enter the last evaluation (0 to 1)",value=0.50),
+       br(),
+       numericInput("project",
+                    "Enter the number of projects-(1 to 10)",value=4),
+       br(),
+       numericInput("worked",
+                    "Enter the number of Average hours worked-(100 to 300)",value=150) ,
+       br(),
+       numericInput("time",
+                    "Enter the time spend at company in years-(1 to 10)",value=3)
+       
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      textOutput("class")
     )
-  )
+  )    
 ))
